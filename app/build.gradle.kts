8e6dc3ed7a8d2518f619499bfeafbbcb785917e1
acascoc098 <acascoc098@g.educaand.es>
2023-10-16 11:06:07 +0200

    Primer apartado, uso de log.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.ejercicio6"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ejercicio6"
--- app/src/main/java/com/example/ejercicio6/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio6/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.ejercicio6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "Apartado 1"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Se llama después de onCreate. Santi, muestro la pantalla. El usuario aún no puede interaccionar")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "El usuario ya puede interaccionar con la pantalla")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Pierdo el foco de la pantalla. Boton home")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Vuelvo a estar visible para santi.")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Dejo de estar visible para el usuario. Otra App, S.O.")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "La actividad muere.")
+    }
+
 }
